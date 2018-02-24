0c1fb1eb7cbf27173e50da472ea08b4c16e9d4dc
Gustavo Decker <gustavdz@gmail.com>
2018-02-24 18:25:07 -0500

    insert y update avanzado con calculos internos

--- app/Http/Controllers/DetFacturasController.php
+++ app/Http/Controllers/DetFacturasController.php
@@ -5,6 +5,7 @@ namespace Products_JWT\Http\Controllers;
 use Illuminate\Http\Request;
 use Products_JWT\detfacturas;
 use JWTAuth;
+use Illuminate\Http\Exceptions;
 
 class DetFacturasController extends Controller
 {
@@ -12,18 +13,43 @@ class DetFacturasController extends Controller
         $detfacturas = detfacturas::where('cabfactura_id', $cabfacturas_id)->get();//->paginate();
         return $detfacturas;
     }
-    public function add(Request $request){
+    public function simple_add(Request $request){
         $detfactura_request = $request->only('detfactura_secuencia','productPrecio','productCantidad','productTotalBruto','productTotalDscto','productTotalTax1','productTotalTax2','productTotalNeto','detfactEstado','products_id','cabfactura_id');
         $detfactura = detfacturas::create($detfactura_request);
         return $detfactura;
+
+    }
+    public function add(Request $request){
+
+        $detfactura_request = $request->only('detfactura_secuencia','productPrecio','productCantidad','productTotalDscto','detfactEstado','products_id','cabfactura_id');
+        $detfactura_request['productTotalBruto']=number_format($detfactura_request['productPrecio']*$detfactura_request['productCantidad'], 2, '.', ',');
+        $detfactura_request['productTotalTax1']=number_format((($detfactura_request['productPrecio']*$detfactura_request['productCantidad'])-$detfactura_request['productTotalDscto'])*0.12, 2, '.', ',');
+        $detfactura_request['productTotalTax2']=number_format((($detfactura_request['productPrecio']*$detfactura_request['productCantidad'])-$detfactura_request['productTotalDscto'])*0.10, 2, '.', ',');
+        $detfactura_request['productTotalNeto']=number_format((($detfactura_request['productPrecio']*$detfactura_request['productCantidad'])-$detfactura_request['productTotalDscto'])+$detfactura_request['productTotalTax1']+$detfactura_request['productTotalTax2'], 2, '.', ',');
+
+        $detfactura = detfacturas::create($detfactura_request);
+        return $detfactura;
+
     }
     public function get($id){
         $detfactura = detfacturas::find($id);
         return $detfactura;
     }
-    public function edit($id, Request $request){
+    public function simple_edit($id, Request $request){
         $detfactura = $this->get($id);
         $detfactura -> fill($request->all())->save();
+        return $detfactura;
+    }
+    public function edit($id, Request $request){
+        $detfactura = $this->get($id);
+
+        $detfactura_request = $request->only('detfactura_secuencia','productPrecio','productCantidad','productTotalDscto','detfactEstado','products_id','cabfactura_id');
+        $detfactura_request['productTotalBruto']=number_format($detfactura_request['productPrecio']*$detfactura_request['productCantidad'], 2, '.', ',');
+        $detfactura_request['productTotalTax1']=number_format((($detfactura_request['productPrecio']*$detfactura_request['productCantidad'])-$detfactura_request['productTotalDscto'])*0.12, 2, '.', ',');
+        $detfactura_request['productTotalTax2']=number_format((($detfactura_request['productPrecio']*$detfactura_request['productCantidad'])-$detfactura_request['productTotalDscto'])*0.10, 2, '.', ',');
+        $detfactura_request['productTotalNeto']=number_format((($detfactura_request['productPrecio']*$detfactura_request['productCantidad'])-$detfactura_request['productTotalDscto'])+$detfactura_request['productTotalTax1']+$detfactura_request['productTotalTax2'], 2, '.', ',');
+
+        $detfactura -> fill($detfactura_request)->save();
         return $detfactura;
     }
     public function delete($id){
