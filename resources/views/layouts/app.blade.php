7a99181602fa05cb4409934b5208e18fb41389bc
Gustavo Decker <gustavdz@gmail.com>
2018-07-01 21:51:53 -0500

    google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,6 +91,16 @@
 
 @include('includes.modal')
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-121710686-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-121710686-1');
+</script>
+
 
 <!-- jQuery -->
 <script src="{{asset('js/jquery/dist/jquery.min.js')}}"></script>
