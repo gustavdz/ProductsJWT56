eeadadd49b33838413d00ad3f1e5d6cf6e7b741b
Gustavo Decker Zambrano <gustavdz@gmail.com>
2018-09-29 11:36:48 -0500

    error mantenimiento template

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,57 +1,15 @@
+@extends('layouts.errors')
+@section('topnavbar','Error 503')
+@section('body-class','nav-md')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Service Unavailable</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <style>
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Raleway', sans-serif;
-            font-weight: 100;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 36px;
-            padding: 20px;
-        }
-    </style>
-</head>
-<body>
-<div class="flex-center position-ref full-height">
-    <div class="content">
-        <div class="title">
-            Be right back.                </div>
+@section('content')
+    <div class="col-md-12">
+        <div class="col-middle">
+            <div class="text-center text-center">
+                <h1 class="error-number">503</h1>
+                <h2>Sitio en mantenimiento</h2>
+                <p>Servicio no disponible por el momento. Intente de nuevo en unos minutos.</p>
+            </div>
+        </div>
     </div>
-</div>
-</body>
-</html>
+@endsection
